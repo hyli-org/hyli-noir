d6a8dc3a42224289e35f6deec398fd567d93e02a
Alexandre Careil <alex.careil.fr@gmail.com>
2025-09-16 19:09:34 +0200

    adapt

--- src/lib.ts
+++ src/lib.ts
@@ -1,5 +1,5 @@
 import * as check_secret from "./check_secret";
 import * as check_jwt from "./check_jwt";
-import * as common from "./common";
+import * as noir_utils from "./common";
 
-export { check_secret, check_jwt, common };
+export { check_secret, check_jwt, noir_utils };
